129e43aafd33b1ef3125cdabd187959ad6c7a45d
euseibus <vladcatoi@gmail.com>
2017-12-05 16:02:51 -0600

    adding rudimentary build and run scripts

---
+++ build.bat
@@ -0,0 +1,3 @@
+cd ..
+lmbr_waf -j 50 build_win_x64_vs2015_profile -p all
+lmbr_waf -j 50 build_win_x64_vs2015_profile_dedicated -p all
---
+++ test.bat
@@ -0,0 +1,8 @@
+../Bin64vc140.Dedicated/TreedIslandLauncher_Server.exe +gm_net_simulator oLatMin:60 oLatMax:150 +gm_disconnectDetection 0 +map tutorial +mphost
+../Bin64vc140/TreedIslandLauncher.exe +mpjoin +windowxy 50 50
+../Bin64vc140/TreedIslandLauncher.exe +mpjoin +windowxy 500 500
+
+
+
+
+
