559ce39ebfe2145d7664cc14b3a840897f191d96
euseibus <vladcatoi@gmail.com>
2017-12-06 16:05:35 -0600

    touched up the batch launcher

--- test.bat
+++ test.bat
@@ -1,6 +1,8 @@
-../Bin64vc140.Dedicated/TreedIslandLauncher_Server.exe +gm_net_simulator oLatMin:60 oLatMax:150 +gm_disconnectDetection 0 +map tutorial +mphost
-../Bin64vc140/TreedIslandLauncher.exe +mpjoin +windowxy 50 50
-../Bin64vc140/TreedIslandLauncher.exe +mpjoin +windowxy 500 500
+start %~dp0..\Bin64vc140.Dedicated\TreedIslandLauncher_Server.exe +gm_net_simulator oLatMin:60 oLatMax:150 +gm_disconnectDetection 0 +map tutorial +mphost
+sleep 5
+start %~dp0..\Bin64vc140\TreedIslandLauncher.exe +mpjoin +windowxy 50 50
+sleep 2
+start %~dp0..\Bin64vc140\TreedIslandLauncher.exe +mpjoin +windowxy 500 500
 
 
 
